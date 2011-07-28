ea481eed681e324c8555a71904f8b06b289e3aee
ReidCarlberg <reid.carlberg@gmail.com>
2011-07-27 17:37:32 -0700

    updated routes so slides are default

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ ForceOsViz1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "options#index"
 
   # See how all your routes lay out with "rake routes"
 
