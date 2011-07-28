88371a88a7098697120fe08199db8fc97848d55e
ReidCarlberg <reid.carlberg@gmail.com>
2011-07-27 17:22:46 -0700

    simple

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ ForceOsViz1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "options#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
