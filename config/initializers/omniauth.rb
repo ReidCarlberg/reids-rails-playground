c8be98a1116baea9de93cd072f4d48c65426e193
ReidCarlberg <reid.carlberg@gmail.com>
2011-07-31 06:16:55 -0500

    updated to omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :salesforce, 
-    '3MVG99OxTyEMCQ3ivs8wbC2A8c1RKWGH16lDCWDv513AJh4g_XFjq1jmACXclk6iU49epvTXBclJNb3lW0lHK', 
-    '3584425645079061440',
+    '3MVG99OxTyEMCQ3ivs8wbC2A8c.NNv8EDMoyw2u2x3D6kErJKWmot4Kkd16H_pbKZkWdwFZs7zay_27rqJtcQ', 
+    '1618732396712108625',
     { :redirect_uri => 'https://cold-moon-137.herokuapp.com/auth/salesforce/callback'}
     
     
