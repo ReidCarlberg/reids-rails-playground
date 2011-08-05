cd9c747865b19e1deadf0b87e6cd3f830ec2447b
ReidCarlberg <reid.carlberg@gmail.com>
2011-08-05 06:03:56 -0500

    minor changes to omniauth.rb comments

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,25 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+    #basic omniauth config
+    #add variables Client Key, Secret and URI as evironmental variables locally
+    #comments here will show up in your logs when the app initializes
+    #note: client options are based on my MBP which seems to have the common problem of Rails server not seeing a CRT file
+    #this is not an issues in production
   
-    client_options = { :ssl => { :ca_file => "/usr/share/curl/curl-ca-bundle.crt" } }
-    redirect_uri = 'http://localhost:3000/auth/salesforce/callback'
-    
-    if Rails.env.production?
-      client_options = nil
-      redirect_uri = 'https://cold-moon-137.herokuapp.com/auth/salesforce/callback'
-    end
-
     puts "************HELLO LOGS!"
-    puts "************Settings"
     puts "Production? ", Rails.env.production?
     puts "Client ", ENV['SALESFORCE_OAUTH2_KEY']
     puts "secret ", ENV['SALESFORCE_OAUTH2_SECRET']
     puts "URI ", ENV['SALESFORCE_OAUTH2_URI']
     puts "options ", client_options
-    #production
-    #provider :salesforce, 
-    #  ENV['SALESFORCE_OAUTH2_KEY'], 
-    #  ENV['SALESFORCE_OAUTH2_SECRET'],
-    #  { :redirect_uri => 'https://cold-moon-137.herokuapp.com/auth/salesforce/callback'}
     
     if Rails.env.production?
       puts "**************production"
